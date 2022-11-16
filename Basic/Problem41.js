baec21a3d77b6b8e1bb4842085f8f24bb8701a45
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-11-16 23:40:43 +0600

    Create Problem41.js

---
+++ Basic/Problem41.js
@@ -0,0 +1,15 @@
+function numberMatchingCheck(n1, n2, n3) {
+    if (n1 == n2 && n2 == n3) {
+        return 30;
+    }
+    else if (n1 == n2 || n1 == n3 || n2 == n3) {
+        return 40;
+    }
+    else {
+        return 20
+    }
+}
+
+console.log(numberMatchingCheck(8, 7, 18));
+console.log(numberMatchingCheck(8, 8, 18));
+console.log(numberMatchingCheck(8, 8, 8));
