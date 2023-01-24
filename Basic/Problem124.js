e1d83e9eb6f32ab6719b3af17f185187c0dacb5d
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2023-01-24 20:26:19 +0600

    Create Problem124.js

---
+++ Basic/Problem124.js
@@ -0,0 +1,6 @@
+function test_logical_Nor(a, b) {
+    return (!a && !b)
+}
+console.log(test_logical_Nor(true, false));
+console.log(test_logical_Nor(false, false));
+console.log(test_logical_Nor(true, true));
