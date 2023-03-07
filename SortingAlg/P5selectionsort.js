21f14e004fe9bf33cce8c5de35e8af9607632220
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2023-03-07 23:43:21 +0600

    P5 solved

---
+++ SortingAlg/P5selectionsort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    return arr;
+}
+
+// Example usage
+const arr = [5, 3, 8, 4, 2];
+console.log(selectionSort(arr)); // [2, 3, 4, 5, 8]
